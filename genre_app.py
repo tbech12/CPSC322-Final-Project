fa1d6c480b70554408ac0333f90b7437ff378e80
tbech12 <troy.bechtold@gmail.com>
2022-04-29 17:20:12 +0000

    fix

--- genre_app.py
+++ genre_app.py
@@ -118,7 +118,7 @@ def load_model(filename):
 
 if __name__ == "__main__":
     # Deployment
-    port = os.environ.get("PORT", 5000)
-    app.run(debug=True, port=port, host="0.0.0.0") # TODO: turn debug off
+    port = os.environ.get("PORT", 5001)
+    app.run(debug=False, port=port, host="0.0.0.0") # TODO: turn debug off
     #app.run(debug=True, port=5000)
     # when deploy to "production"
